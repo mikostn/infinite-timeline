0cfc0d87c6811c96b74395e4940c84308b5459b1
miko <web.m@stn.pl>
2015-12-10 12:02:05 +0100

    add init and resize year list func  with delay

--- js/infinite-timeline.js
+++ js/infinite-timeline.js
@@ -83,7 +83,15 @@ jQuery(function () {
         }
 
         infinite_timeline_adjust_vertical_position(0);
-        updateYearList();
+    });
+    // run init func with delay, wiat for resize to finish, + wait for css animations to finish!
+    var resizeTimer;
+    $(window).on('load resize', function (e) {
+        clearTimeout(resizeTimer);
+        resizeTimer = setTimeout(function () {
+            // Run code here, resizing has "stopped"
+            setColorInits();
+        }, 400);
     });
 });
 
